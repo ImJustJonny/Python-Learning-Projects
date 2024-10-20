1e245950341a92a9db443596b1d24b7be87f0620
Jonathon Russell <113649337+ImJustJonny@users.noreply.github.com>
2024-10-20 01:27:27 -0400

    Update oddoreven.py

--- projects/oddoreven.py
+++ projects/oddoreven.py
@@ -3,7 +3,7 @@
 #User Input
 user_input = int(input("What is your number?: "))
 
-#If statements to see if the number divides evenly into two, or
+#If statements to check is the modulo left over is 0 (even) or 1 (odd).
 if user_input % 2 == 0:
     print("Your number is even.")
 elif user_input % 2 == 1:
